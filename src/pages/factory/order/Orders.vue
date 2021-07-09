a384af22d3f96663700d91a2531cfbc5ce84b365
Iynga Iyngaran Iyathurai <iynga@sas.co.nz>
2021-07-09 11:59:57 +0530

    wip

--- src/pages/factory/order/Orders.vue
+++ src/pages/factory/order/Orders.vue
@@ -12,7 +12,7 @@
 
         <li class="flex items-center">
           <router-link :to="{ name: 'DesignersDashboard' }" class="text-gray-700">
-            Designer
+            Factory
           </router-link>
           <svg class="fill-current w-3 h-3 mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"/></svg>
         </li>
@@ -20,7 +20,7 @@
 
         <li class="flex items-center">
           <router-link :to="{ name: 'DesignRequests' }" class="text-gray-600">
-            Design Requests
+            Orders
           </router-link>
         </li>
 
@@ -30,7 +30,7 @@
 
     <div class="flex flex-wrap">
       <div class="w-full">
-        <p class="text-xl font-semibold mb-4">Design Requests</p>
+        <p class="text-xl font-semibold mb-4">Orders</p>
         <div class="w-full bg-white border text-blue-400 rounded-lg flex items-center pt-6 pb-6 pl-6 pr-1 mb-6 xl:mb-0">
           <el-table
               :data="factoryOrders.data"
@@ -54,11 +54,6 @@
             <el-table-column
                 prop="total"
                 label="Total">
-            </el-table-column>
-
-            <el-table-column
-                prop="status"
-                label="Status">
             </el-table-column>
 
 
